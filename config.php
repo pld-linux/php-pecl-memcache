f8ec07ca8beeb877395e2272d697ba659664c727
Elan Ruusamäe <glen@pld-linux.org>
2010-03-25 09:34:19 +0000

    - config extracted from memcached.php
    Changed files:
        config.php -> 1.1

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Config for memcache.php administrative interface.
+ */
+
+// Admin Username
+define('ADMIN_USERNAME', 'memcache');
+// Admin Password
+define('ADMIN_PASSWORD', 'memcache');
+
+define('DATE_FORMAT', 'Y/m/d H:i:s');
+define('GRAPH_SIZE', 200);
+define('MAX_ITEM_DUMP', 50);
+
+// add more as an array
+$MEMCACHE_SERVERS[] = 'localhost:11211';
