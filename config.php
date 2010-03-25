41fe180ba0a3d04bf54afeb206f76f5145a15663
Elan Ruusamäe <glen@pld-linux.org>
2010-03-25 09:34:42 +0000

    - use iso8601 date format to avoid ambiguity
    Changed files:
        config.php -> 1.2

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ define('ADMIN_USERNAME', 'memcache');
 // Admin Password
 define('ADMIN_PASSWORD', 'memcache');
 
-define('DATE_FORMAT', 'Y/m/d H:i:s');
+define('DATE_FORMAT', 'Y-m-d H:i:s');
 define('GRAPH_SIZE', 200);
 define('MAX_ITEM_DUMP', 50);
 
